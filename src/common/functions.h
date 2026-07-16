563a2ad13533375bcad8169f7c7880825ddc0ca4
FedeLupianez <lupianezfederico@gmail.com>
2026-07-16 16:27:21 -0300

    add logging function

--- src/common/functions.h
+++ src/common/functions.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <time.h>
 #define KB(n) (n << 10)
 #define MB(n) (n << 20)
 #define GB(n) (n << 30)
+
+void logging(char* level, char* msg, ...)
+{
+    time_t now = time(NULL);
+    fprintf(stderr, "%ld %s:", now, level);
+    va_list args;
+    va_start(args, msg);
+    vfprintf(stderr, msg, args);
+    va_end(args);
+    fprintf(stderr, "\n");
+}
